e3135f592b77bf512bf1e289379eaf64ef98a3e4
alcck <99951322+alcck@users.noreply.github.com>
2023-12-30 17:49:01 +0300

    Add files via upload
    Second commit

--- demo.py
+++ demo.py
@@ -1,66 +1,65 @@
-import tkinter as tk
+from mpi4py import MPI
 import networkx as nx
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from mpi4py import MPI
 
-class GraphApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Ranked Vertex Coloring Algorithm")
-        self.canvas = None
+class GraphColoring:
+    def __init__(self, graph, comm):
+        self.graph = graph
+        self.comm = comm
+        self.rank = comm.Get_rank()
+        self.size = comm.Get_size()
+        
 
-        self.G = nx.Graph()
-        self.G.add_edges_from([(6, 3), (6, 2), (3, 2), (2, 4), (3, 4), (3, 0), (0, 4), (4, 5), (4, 7), (0, 7), (7, 5), (7, 1), (5, 1)])
+    def get_connected_components(self):
+        components = []
+        for component_nodes in nx.connected_components(self.graph):
+            components.append(set(component_nodes))
+        return components
 
-        # Initialize colors for nodes
-        self.color_map = [None] * 8
-        self.step = 0
+    def color_connected_components(self):
+        components = self.get_connected_components()
+        for round_number in range(len(components)):
+            if round_number % self.size == self.rank:
+                for comp_idx, comp_nodes in enumerate(components):
+                    if comp_idx == round_number:
+                        self.color_component(comp_nodes)
 
-        # Initialize MPI
-        self.comm = MPI.COMM_WORLD
-        self.rank = self.comm.Get_rank()
-        self.size = self.comm.Get_size()
+    def color_component(self, component_nodes):
+        # Sort nodes by rank in descending order
+        sorted_nodes = sorted(component_nodes, key=lambda node: -node)
 
-        self.update_graph()
+        color_map = {}
 
-    def update_graph(self):
-        if self.step < 8:
-            # Update color for the next node
-            self.color_map[self.step] = self.get_available_color(self.step)
-            self.step += 1
+        for node in sorted_nodes:
+            available_colors = set(range(len(self.graph.nodes)))
+            for neighbor in self.graph.neighbors(node):
+                neighbor_color = color_map.get(neighbor, None)
+                if neighbor_color in available_colors:
+                    available_colors.remove(neighbor_color)
+            if available_colors:
+                color_map[node] = min(available_colors)
 
-            # Broadcast color_map to all nodes
-            self.color_map = self.comm.bcast(self.color_map, root=0)
+        colors_list = [color_map.get(node, None) for node in self.graph.nodes]
+        self.graph.graph["coloring"] = colors_list
 
-            # Draw the updated graph
-            plt.clf()
-            pos = nx.spring_layout(self.G, seed=42)
-            nx.draw(self.G, pos, node_color=self.color_map, with_labels=True, font_size=10, node_size=500)
-            plt.title(f"Step {self.step}")
-            plt.axis("off")
-
-            # Display the graph in Tkinter window
-            if self.canvas:
-                self.canvas.get_tk_widget().destroy()
-            self.canvas = FigureCanvasTkAgg(plt.gcf(), master=self.root)
-            self.canvas.draw()
-            self.canvas.get_tk_widget().pack()
-
-            # Schedule the next update
-            self.root.after(1000, self.update_graph)
-        else:
-            plt.close()  # Close the plot when all steps are done
-
-    def get_available_color(self, vertex):
-        # Find the lowest available color for the given vertex
-        used_colors = set(self.color_map[neighbor] for neighbor in self.G.neighbors(vertex))
-        for color in range(len(self.G)):
-            if color not in used_colors:
-                return color
-        return None
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = GraphApp(root)
-    root.mainloop()
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    # Given edge list
+    edges = [(6, 3), (6, 2), (3, 2), (2, 4), (3, 4), (3, 0), (0, 4), (4, 5), (4, 7), (0, 7), (7, 5), (7, 1), (5, 1)]
+
+    # Create a graph
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    coloring = GraphColoring(G, comm)
+    coloring.color_connected_components()
+
+    if rank == 0:
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, node_color=G.graph["coloring"], with_labels=True, font_size=10, node_size=500)
+        plt.title("Colored Graph")
+        plt.axis("off")
+        plt.show()
